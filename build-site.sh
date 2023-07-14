0fea44c680ae6760d9b36cff50d18798adf439c6
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2023-07-13 22:05:19 -0400

    rename output file and clear when done

--- build-site.sh
+++ build-site.sh
@@ -5,5 +5,6 @@ pip install -U pip
 pip install -Ur paper-requirements.txt
 jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace paper.ipynb
 jupyter nbconvert --execute --to notebook --inplace paper.ipynb
-jupyter nbconvert paper.ipynb --to html --template classic --output-dir _site
+jupyter nbconvert paper.ipynb --to html --template classic --output-dir _site --output index
 ghp-import -n -p -f _site
+jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace paper.ipynb
